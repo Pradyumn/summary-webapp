0fcea449d92ef474637e62c3dc27b74a872afd1f
Pradyumn <spradyumn79@gmail.com>
2021-05-16 22:59:02 +0530

    silent output wget

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 mkdir -p ./utils/saved_model && 
 cd ./utils/saved_model &&
-wget https://transfer.sh/aKdbO/checkpoint &&
-wget https://transfer.sh/Obch6/model.ckpt-297190.data-00000-of-00001 &&
-wget https://transfer.sh/n5q9s/model.ckpt-297190.index &&
-wget https://transfer.sh/ehez2/model.ckpt-297190.meta
+wget --quiet https://transfer.sh/aKdbO/checkpoint &&
+wget --quiet https://transfer.sh/Obch6/model.ckpt-297190.data-00000-of-00001 &&
+wget --quiet https://transfer.sh/n5q9s/model.ckpt-297190.index &&
+wget --quiet https://transfer.sh/ehez2/model.ckpt-297190.meta
